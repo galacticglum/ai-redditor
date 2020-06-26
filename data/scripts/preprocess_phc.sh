eddbb2c606d95b465369409c4f78c879b84d6724
GalacticGlum <nerftactic09@gmail.com>
2020-06-26 13:51:02 -0400

    Staging

--- data/scripts/preprocess_phc.sh
+++ data/scripts/preprocess_phc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 
-# A script for preproceessing posts scraped from r/writingprompts.
-# Usage: ./preprocess_writingprompts.sh <input_file> <output_file>
+# A script for preproceessing pornhub commennts.
+# Usage: ./preprocess_phc.sh <input_file> <output_file>
 
 if [ '$0' != '' && '$1' != '' ];  then
     python ai-redditor\preprocess.py $0 $1 --format "<|bos|><|bol|>{likes}<|eol|><|bon|>{author}<|eon|><|eq_tok|>{body}<|eos|>" --dataset-ratio 1 --shuffle --test-split-ratio 0.10
