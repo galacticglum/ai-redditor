591890c80e1b674b844964841ca17ca6a934df8c
GalacticGlum <nerftactic09@gmail.com>
2020-07-03 10:29:01 -0400

    Added generate text routes

--- web_service/ai_redditor_service/routes/main.py
+++ web_service/ai_redditor_service/routes/main.py
@@ -10,10 +10,22 @@ def index():
 def tifu_page():
     return render_template('tifu.html')
 
+@bp.route('/tifu/generate')
+def tifu_generate_page():
+    return render_template('tifu_generate.html')
+
 @bp.route('/wp')
 def writingprompts_page():
     return render_template('writingprompts.html')
 
+@bp.route('/wp/generate')
+def writingprompts_generate_page():
+    return render_template('writingprompts_generate.html')
+
 @bp.route('/phc')
 def phc_page():
     return render_template('phc.html')
+
+@bp.route('/phc/generate')
+def phc_generate_page():
+    return render_template('phc_generate.html')
