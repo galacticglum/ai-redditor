31c9de61b228ccd4e6acc2b5429107489770abf0
GalacticGlum <nerftactic09@gmail.com>
2020-07-04 23:21:22 -0400

    Added randomized likes in phc fixture creation if no likes are specified

--- web_service/scripts/create_phc_fixture.py
+++ web_service/scripts/create_phc_fixture.py
@@ -4,7 +4,9 @@ Creates a PHCRecord fixture from generated samples.
 '''
 
 import re
+import time
 import json
+import random
 import argparse
 from pathlib import Path
 from create_fixture_utils import get_special_tokens_match_pattern
@@ -16,8 +18,21 @@ parser.add_argument('--special-tokens', dest='special_tokens_map_filename', type
                     default=None, help='A JSON file containing a dictionary of special tokens.')
 parser.add_argument('--no-indent-json', dest='indent_json', action='store_false',
                     help='Don\'t indent the output JSON file. Default behaviour is to indent.')
-parser.add_argument('--default-likes', type=int, default=0, help='The default likes on a post if none is provided.')
+parser.add_argument('--default-likes-min', type=int, default=0, help='The minimum range on default likes.')
+parser.add_argument('--default-likes-max', type=int, default=25, help='The maximum range on default likes.')
+parser.add_argument('--seed', type=int, default=None, help='The seed of the random engine.')
 args = parser.parse_args()
+
+# Set seed to reproduce results.
+if args.seed is None:
+    # We use the current time as the seed rather than letting numpy seed
+    # since we want to achieve consistent results across sessions.
+    # Source: https://stackoverflow.com/a/45573061/7614083
+    t = int(time.time() * 1000.0)
+    args.seed = ((t & 0xff000000) >> 24) + ((t & 0x00ff0000) >> 8) + ((t & 0x0000ff00) <<  8) + ((t & 0x000000ff) << 24)
+
+random.seed(args.seed)
+print('- Set seed to {}'.format(args.seed))
 
 special_tokens_match_pattern = get_special_tokens_match_pattern(args.special_tokens_map_filename)
 with open(args.input_filename) as input_file, \
@@ -32,10 +47,10 @@ with open(args.input_filename) as input_file, \
         # likes string, since its an integer value.
         likes = re.sub('[^0-9]', '', sample['groups']['likes'])
         if not likes:
-            likes = args.default_likes
+            likes = random.randint(args.default_likes_min, args.default_likes_max)
         else:
             likes = int(likes)
-
+            
         output_data.append({
             'likes': likes,
             'author_username': author_username,
