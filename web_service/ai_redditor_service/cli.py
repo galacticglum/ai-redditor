737c66e17862f6473e993db59a0606dda2134132
GalacticGlum <nerftactic09@gmail.com>
2020-07-04 21:21:21 -0400

    Started fixture loading command * A fixture is the JSON-representation of a model. This command is used   for loading the pre-generated record sets into the database.

--- web_service/ai_redditor_service/cli.py
+++ web_service/ai_redditor_service/cli.py
@@ -1,6 +1,9 @@
+import json
 import click
+from pathlib import Path
 from flask.cli import with_appcontext
 from ai_redditor_service.extensions import db
+from ai_redditor_service.models import TIFURecord, WPRecord, PHCRecord
 
 def init_app(app):
     '''
@@ -12,6 +15,7 @@ def init_app(app):
     '''
 
     app.cli.add_command(_init_db_command)
+    app.cli.add_command(_load_fixture_command)
 
 @click.command('init-db')
 @with_appcontext
@@ -21,3 +25,24 @@ def _init_db_command():
         db.create_all()
         click.echo('Initialized the database: dropped and recreated all tables.')  
 
+def _load_tifu_fixture():
+    pass
+
+def _load_wp_fixture():
+    pass
+
+def _load_phc_fixture():
+    pass
+
+_TYPE_HANDLER_MAP = {
+    'tifu': _load_tifu_fixture,
+    'wp': _load_wp_fixture,
+    'phc': _load_phc_fixture
+}
+
+@click.command('load-fixture')
+@click.argument('record_type', type=click.Choice(_TYPE_HANDLER_MAP.keys(), case_sensitive=False))
+@click.argument('fixture_filename', type=Path)
+@with_appcontext
+def _load_fixture_command(record_type, fixture_filename):
+    _TYPE_HANDLER_MAP[record_type]()
