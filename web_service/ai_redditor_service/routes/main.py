f7be62d3c01c361b594dfecf796eae71c603ff14
GalacticGlum <nerftactic09@gmail.com>
2020-07-05 09:31:31 -0400

    Added random non-custom record selection * TODO: Index is_custom row for better performance

--- web_service/ai_redditor_service/routes/main.py
+++ web_service/ai_redditor_service/routes/main.py
@@ -1,7 +1,13 @@
-from ai_redditor_service.forms import GeneratePostForm
+from sqlalchemy import func
 from flask import Blueprint, redirect, url_for, render_template
 
+from ai_redditor_service.forms import GeneratePostForm
+from ai_redditor_service.models import TIFURecord, WPRecord, PHCRecord
+
 bp = Blueprint('main', __name__, url_prefix='/')
+
+def _select_random(model_class, **filter_kwargs):
+    return model_class.query.filter_by(**filter_kwargs).order_by(func.random()).first()
 
 @bp.route('/')
 def index():
@@ -14,11 +20,18 @@ def tifu_page():
         # TODO: Use form data to generate post
         pass
 
-    return render_template('tifu.html', generate_form=generate_form)
+    record = _select_random(TIFURecord, is_custom=False)
+    return render_template('tifu.html', record=record, generate_form=generate_form)
 
 @bp.route('/wp')
 def writingprompts_page():
-    return render_template('writingprompts.html')
+    generate_form = GeneratePostForm()
+    if generate_form.validate_on_submit():
+        # TODO: Use form data to generate post
+        pass
+
+    record = _select_random(WPRecord, is_custom=False)
+    return render_template('writingprompts.html', record=record, generate_form=generate_form)
 
 @bp.route('/phc')
 def phc_page():
