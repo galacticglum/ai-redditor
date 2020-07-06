9f93f50b68e35ee841cd20f6467d81401a90ee3a
GalacticGlum <nerftactic09@gmail.com>
2020-07-06 15:33:12 -0400

    Staging

--- web_service/ai_redditor_service/forms/generate_post_form.py
+++ web_service/ai_redditor_service/forms/generate_post_form.py
@@ -8,15 +8,7 @@ class GeneratePostForm(FlaskForm):
 
     :note:
         A standard Reddit post, in reference to this model, is in
-        a query-answer format. This format can be thought of a pair
-        of strings (referred to as the query and answer respectively)
-        where the answer is a mapping of the query into some other
-        translation space. For example, the query might be a sentence in
-        English and the answer might be the same sentence translated to German.
-
-        In the case of Reddit post text generation, the query is the title of the post
-        (such as a writingprompt) and the answer is the corresponding post selftext or
-        comment (such as a story written from the prompt).
+        a query-answer format.
 
     '''
 
