65c267c6dfc98a28a02e067c41ac43697cf66c59
GalacticGlum <nerftactic09@gmail.com>
2020-07-16 15:31:34 -0400

    Loading next record while results are displayed

--- guessing_game/src/App.js
+++ guessing_game/src/App.js
@@ -5,6 +5,23 @@ import HomePage from './pages/HomePage';
 import GamePage from './pages/game/GamePage';
 import './App.css';
 
+import axios from 'axios';
+
+// Add response time
+axios.interceptors.request.use(x => {
+    x.meta = x.meta || {};
+    x.meta.startTime = new Date().getTime();
+    return x;
+});
+
+axios.interceptors.response.use(x => {
+    x.duration = new Date().getTime() - x.config.meta.startTime;
+    return x;
+}, x => {
+    x.duration = new Date().getTime() - x.config.meta.startTime;
+    throw x;
+})
+
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export default class App extends Component {
