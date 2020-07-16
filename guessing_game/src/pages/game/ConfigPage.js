14eeae34ca5230560bcde1df8b372decf5eb9a8d
GalacticGlum <nerftactic09@gmail.com>
2020-07-15 20:12:13 -0400

    Refactored game page and config page * Added error alert * Added record fetching using axios

--- guessing_game/src/pages/game/ConfigPage.js
+++ guessing_game/src/pages/game/ConfigPage.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import RangeSlider from 'react-bootstrap-range-slider';
 import Button from 'react-bootstrap/Button';
@@ -64,68 +61,62 @@ export default class ConfigPage extends Component {
     }
 
     render() {
-        return (
-            <Container>
-                <Row>
-                    <Col sm="12" md="8" lg="6" className="mx-auto">
-                        <div className="settings-panel">
-                            <h4 className="text-center mt-2 font-weight-bold">settings</h4>
-                            <Form className="mt-4">
-                                <Form.Group>
-                                    <Form.Label className="font-weight-bold">types of posts?</Form.Label>
-                                    <div>
-                                        <Form.Check id="tifu" inline custom className="record-type-checkbox">
-                                            <Form.Check.Input type="checkbox"
-                                                onChange={this.recordTypeToggleChange}
-                                                checked={this.state.recordTypes.tifu} />
-                                            <WithTooltip text="today i fucked up">
-                                                <Form.Check.Label>tifu</Form.Check.Label>
-                                            </WithTooltip>
-                                        </Form.Check>
-                                        <Form.Check id="wp" inline custom className="record-type-checkbox">
-                                            <Form.Check.Input type="checkbox"
-                                                onChange={this.recordTypeToggleChange}
-                                                checked={this.state.recordTypes.wp} />
-                                            <WithTooltip text="writing prompts">
-                                                <Form.Check.Label>wp</Form.Check.Label>
-                                            </WithTooltip>
-                                        </Form.Check>
-                                        <Form.Check id="phc" inline custom className="record-type-checkbox">
-                                            <Form.Check.Input type="checkbox"
-                                                onChange={this.recordTypeToggleChange}
-                                                checked={this.state.recordTypes.phc} />
-                                            <WithTooltip text="pornhub comments">
-                                                <Form.Check.Label>phc</Form.Check.Label>
-                                            </WithTooltip>
-                                        </Form.Check>  
-                                    </div>
-                                </Form.Group>
-                                <Form.Group className="mt-4">
-                                    <Form.Check id="max-guessing-time-toggle" custom className="record-type-checkbox">
-                                        <Form.Check.Input type="checkbox" 
-                                            onChange={this.maxGuessingTimeToggleChange}
-                                            checked={this.state.maxGuessingTimeEnabled} />
-                                        <Form.Check.Label className="font-weight-bold"
-                                            disabled={!this.state.maxGuessingTimeEnabled}
-                                        >
-                                            max guessing time (seconds)
-                                        </Form.Check.Label>
-                                    </Form.Check>
-                                    <RangeSlider disabled={!this.state.maxGuessingTimeEnabled} value={this.state.maxGuessingTime}
-                                        min="0" max="60" className="max-guessing-time-range-slider"
-                                        onChange={this.maxGuessingTimeChange}
-                                    />
-                                </Form.Group>
-                                <div className="text-center mt-4">
-                                    <Button size="lg" className="ready-btn" onClick={() => this.props.action(this.state)}>
-                                        ready!
-                                    </Button>    
-                                </div> 
-                            </Form>
+        return (    
+            <div className="settings-panel">
+                <h4 className="text-center mt-2 font-weight-bold">settings</h4>
+                <Form className="mt-4">
+                    <Form.Group>
+                        <Form.Label className="font-weight-bold">types of posts?</Form.Label>
+                        <div>
+                            <Form.Check id="tifu" inline custom className="record-type-checkbox">
+                                <Form.Check.Input type="checkbox"
+                                    onChange={this.recordTypeToggleChange}
+                                    checked={this.state.recordTypes.tifu} />
+                                <WithTooltip text="today i fucked up">
+                                    <Form.Check.Label>tifu</Form.Check.Label>
+                                </WithTooltip>
+                            </Form.Check>
+                            <Form.Check id="wp" inline custom className="record-type-checkbox">
+                                <Form.Check.Input type="checkbox"
+                                    onChange={this.recordTypeToggleChange}
+                                    checked={this.state.recordTypes.wp} />
+                                <WithTooltip text="writing prompts">
+                                    <Form.Check.Label>wp</Form.Check.Label>
+                                </WithTooltip>
+                            </Form.Check>
+                            <Form.Check id="phc" inline custom className="record-type-checkbox">
+                                <Form.Check.Input type="checkbox"
+                                    onChange={this.recordTypeToggleChange}
+                                    checked={this.state.recordTypes.phc} />
+                                <WithTooltip text="pornhub comments">
+                                    <Form.Check.Label>phc</Form.Check.Label>
+                                </WithTooltip>
+                            </Form.Check>  
                         </div>
-                    </Col>
-                </Row>
-            </Container>
+                    </Form.Group>
+                    <Form.Group className="mt-4">
+                        <Form.Check id="max-guessing-time-toggle" custom className="record-type-checkbox">
+                            <Form.Check.Input type="checkbox" 
+                                onChange={this.maxGuessingTimeToggleChange}
+                                checked={this.state.maxGuessingTimeEnabled} />
+                            <Form.Check.Label className="font-weight-bold"
+                                disabled={!this.state.maxGuessingTimeEnabled}
+                            >
+                                max guessing time (seconds)
+                            </Form.Check.Label>
+                        </Form.Check>
+                        <RangeSlider disabled={!this.state.maxGuessingTimeEnabled} value={this.state.maxGuessingTime}
+                            min="0" max="60" className="max-guessing-time-range-slider"
+                            onChange={this.maxGuessingTimeChange}
+                        />
+                    </Form.Group>
+                    <div className="text-center mt-4">
+                        <Button size="lg" className="ready-btn" onClick={() => this.props.action(this.state)}>
+                            ready!
+                        </Button>    
+                    </div> 
+                </Form>
+            </div>         
         )
     }
 }
--- guessing_game/src/pages/game/GamePage.js
+++ guessing_game/src/pages/game/GamePage.js
@@ -1,30 +1,113 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react';
 import ConfigPage from './ConfigPage';
+import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './GamePage.css';
+
+import axios from 'axios';
+import { API_BASE_URL } from '../../App';
+
+function DismissibleAlert(props) {
+    const [show, setShow] = useState(true);
+
+    if (show) {
+        return (
+            <Alert {...props} onClose={() => setShow(false)} dismissible>
+                {props.children}
+            </Alert>
+        )
+    }
+
+    return null;
+}
 
 export default class GamePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isConfigPageVisible: true
+            isConfigPageVisible: true,
+            hasError: false,
         };
     }
 
     onConfigPageReady = (configState) => {
+        // Map the record types from a (string, bool) key/value pair to
+        // an array of strings indicating the types to sample from.
+        let recordTypes = [];
+        for (const [key, value] of Object.entries(configState.recordTypes)) {
+            if (!value) continue;
+            recordTypes.push(key);
+        }
+
         this.setState({
-            gameConfig: configState,
-            isConfigPageVisible: false
+            gameConfig: {
+                ...configState,
+                recordTypes: recordTypes
+            }
+        }, () => {
+            this.nextRecord();
+        });
+    }
+
+    nextRecord = () => {
+        // Randomly select record type
+        const recordTypes = this.state.gameConfig.recordTypes;
+        const recordType = recordTypes[Math.floor(Math.random() * recordTypes.length)];
+        this.fetchRecord(recordType, false);
+    }
+
+    fetchRecord = (recordType, isGenerated) => {
+        const requestData = {
+            // A "custom" record refers to a user-generated record
+            'is_custom': false,
+            // If a record is not generated, it means it was not 'written'
+            // by the GPT2 model (i.e. it is written by a human).
+            'is_generated': isGenerated,
+        };
+
+        this.setState({hasError: false});
+        axios.post(`${API_BASE_URL}/r/${recordType}/random`, {
+            ...requestData,    
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            this.setState({
+                currentRecord: response.data
+            });
+        })
+        .catch(error => {
+            this.setState({hasError: true});
+            console.log(error);
         });
     }
 
     render() {
-        if (this.state.isConfigPageVisible) {
-            return ( <ConfigPage action={this.onConfigPageReady} /> );
-        }
-        
         return (
-            <div>
-
-            </div>
+            <Container>
+                <Row>
+                    <Col sm="12" md="8" lg="6" className="mx-auto">
+                        <div id="view-wrapper">
+                            {
+                                (this.state.hasError ? 
+                                    (<DismissibleAlert variant="danger" className="error-alert">
+                                        <Alert.Heading>server go brrr</Alert.Heading>
+                                        This is probably an issue with our servers. Please try again later.
+                                    </DismissibleAlert>                )
+                                : null)
+                            }
+                            {
+                                (this.state.currentRecord === undefined ? 
+                                    (<ConfigPage action={this.onConfigPageReady} />)
+                                : null)
+                            }
+                        </div>    
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
