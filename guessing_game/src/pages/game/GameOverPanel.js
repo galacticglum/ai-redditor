96207d5106a537d75120bb769bdcd5b16d7627ab
GalacticGlum <nerftactic09@gmail.com>
2020-07-17 19:32:56 -0400

    Added quit and play again functionality to game over panel

--- guessing_game/src/pages/game/GameOverPanel.js
+++ guessing_game/src/pages/game/GameOverPanel.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import './GameOverPanel.css';
 
-export default class GameOverPanel extends Component {
+class GameOverPanel extends Component {
+    onQuitButtonClicked = () => {
+        // Redirect to home page
+        this.props.history.push('/');
+    }
+
     render() {
         return (
             <div className="panel">
                 <h4 className="text-center mt-2 font-weight-bold">{this.props.title}</h4>
                 {this.props.content}
                 <div className="d-flex flex-row mt-4">
-                    <Button className="w-100 mr-3 action-btn action-btn-quit" size="lg">
+                    <Button size="lg" onClick={this.onQuitButtonClicked}
+                        className="w-100 mr-3 action-btn action-btn-quit"
+                        disabled={this.props.disabled}
+                    >
                         quit
                     </Button>
-                    <Button className="w-100 action-btn action-btn-restart" size="lg">
+                    <Button size="lg" onClick={this.props.onPlayAgain}
+                        className="w-100 action-btn action-btn-restart"
+                        disabled={this.props.disabled}
+                    >
                         play again
                     </Button>
                 </div> 
@@ -20,3 +32,9 @@ export default class GameOverPanel extends Component {
         )
     }
 }
+
+GameOverPanel.defaultProps = {
+    disabled: false
+}
+
+export default withRouter(GameOverPanel);
