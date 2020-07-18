63db5a3e1771e5847f90b8b3c07d2346d26ac6b2
GalacticGlum <nerftactic09@gmail.com>
2020-07-18 13:18:28 -0400

    Added wsgi.py

---
+++ web_service/ai_redditor_service/wsgi.py
@@ -0,0 +1,12 @@
+'''
+The entrypoint for uWSGI.
+
+'''
+
+from ai_redditor_service import create_app
+
+# Initialize the app
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
